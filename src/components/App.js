2b2a00f22b962d85efb2acca94a6e8e7b3a6eedc
HSM256 <harsha.katakam123@gmail.com>
2020-10-19 17:52:00 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    const relativesList = ["Harsha", "Akhil", "Ram", "Hello"];
+    return (
+      <>
+        <ol>
+          {relativesList.map((m) => (
+            <li key={`relativeList${m}`}></li>
+          ))}
+        </ol>
+      </>
+    );
+  }
 }
-
 
 export default App;
